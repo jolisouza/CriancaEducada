16ea31fafca73dd05e4164222b1c84ca3f8e9e87
joliathan souza <coelhodesouzajoliathan1@gmail.com>
2021-09-30 12:52:46 -0300

    resolução do exercício.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace CriancaEducada
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+
+            string frase, fraseCensurada;
+
+            Console.WriteLine("Olá, Usuário!");
+            Console.WriteLine();
+
+            Console.Write("Digite uma frase: ");
+            frase = Console.ReadLine();
+            Console.WriteLine();
+
+            fraseCensurada = frase.Replace("chato", "#@$%*!&")
+                .Replace("chata", "#@$%*!&")
+                .Replace("bobo", "#@$%*!&")
+                .Replace("boba", "#@$%*!&")
+                .Replace("feio", "#@$%*!&")
+                .Replace("feia", "#@$%*!&")
+                .Replace("boboca", "#@$%*!&")
+                .Replace("bocó", "#@$%*!&")
+                .Replace("tonto", "#@$%*!&")
+                .Replace("tonta", "#@$%*!&")
+                .Replace("palerma", "#@$%*!&")
+                .Replace("paspalho", "#@$%*!&")
+                .Replace("paspalha", "#@$%*!&")
+                .Replace("tantã", "#@$%*!&")
+                .Replace("panaca", "#@$%*!&")
+                .Replace("pentelho", "#@$%*!&")
+                .Replace("pentelha", "#@$%*!&")
+                .Replace("burro", "#@$%*!&")
+                .Replace("burra", "#@$%*!&")
+                .Replace("besta", "#@$%*!&");
+
+            Console.WriteLine($"Frase \"consertada\": \n{fraseCensurada}");
         }
     }
 }
